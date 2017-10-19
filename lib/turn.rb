554a7f5f44f638c89e737ba7e904473a9ff915e6
Brent  Ransom <sadumedusa@gmail.com>
2017-10-19 04:37:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ end
 def valid_move?(board, index)
   if board[index] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     return true
-  elsif index == 4
+  elsif index = 4
     return false
   else
     return false
